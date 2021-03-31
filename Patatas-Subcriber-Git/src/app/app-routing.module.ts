569d17487e86b74d6cf59381619db1cf1add4aa2
Alejandro Torres <joalto97@outlook.com>
2021-03-31 06:55:48 -0500

    removing the default HTML from angular and creating the routes and importing them to the module

--- Patatas-Subcriber-Git/src/app/app-routing.module.ts
+++ Patatas-Subcriber-Git/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Views/login/login.component';
+import { ListSubcribersComponent } from './Views/list-subcribers/list-subcribers.component';
+import { SubscriberDetailComponent } from './Views/subscriber-detail/subscriber-detail.component';
+import { NewComponent } from './Views/new/new.component';
+import { EditComponent } from './Views/edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'Login', pathMatch: 'full'},
+  {path: 'Login', component: LoginComponent},
+  {path: 'Subscribers', component: ListSubcribersComponent},
+  {path: 'New', component: NewComponent},
+  {path: 'Detail/:Id', component: SubscriberDetailComponent},
+  {path: 'Edit/:Id', component: EditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const ComponentPath = [
+  LoginComponent,
+  ListSubcribersComponent,
+  NewComponent,
+  SubscriberDetailComponent,
+  EditComponent
+]
--- Patatas-Subcriber-Git/src/app/app.module.ts
+++ Patatas-Subcriber-Git/src/app/app.module.ts
@@ -1,26 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, ComponentPath } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ViewsComponent } from './Login/views/views.component';
-import { LoginComponent } from './Views/login/login.component';
-import { ListSubcribersComponent } from './Views/list-subcribers/list-subcribers.component';
-import { SubscriberDetailComponent } from './Views/subscriber-detail/subscriber-detail.component';
-import { NewComponent } from './Views/new/new.component';
-import { EditComponent } from './Views/edit/edit.component';
 import { HeadersComponent } from './Templates/headers/headers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewsComponent,
-    LoginComponent,
-    ListSubcribersComponent,
-    SubscriberDetailComponent,
-    NewComponent,
-    EditComponent,
-    HeadersComponent
+    HeadersComponent,
+    ComponentPath
   ],
   imports: [
     BrowserModule,
